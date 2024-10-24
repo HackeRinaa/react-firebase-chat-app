234969f337b7adee9288c987bbf78c665802377b
HackeRinaa <marina03@outlook.com.gr>
2024-10-24 10:45:02 +0300

    feat : chatlist and main section design

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+
+const Chat = () => {
+  const [open,setOpen] = useState(false);
+  const [text,setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText(prev=>prev+e.emoji);
+    setOpen(false);
+  };
+
+    return (
+      <div className="chat">
+        <div className="top">
+          <div className="user">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <span>Jane Doe</span>
+              <p>Lorem ispum dolor , sit amet</p>
+            </div>
+          </div>
+          <div className="icons">
+            <img src="./phone.png" alt="" />
+            <img src="./video.png" alt="" />
+            <img src="./info.png" alt="" />
+          </div>
+        </div>
+
+        <div className="center">
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ante suscipit bibendum ultricies. Praesent at nisi ut lorem vestibulum suscipit sed quis erat. Nunc lacinia iaculis urna sed bibendum. Pellentesque id nulla nulla. Praesent vitae vestibulum turpis. Duis elit nisl, cursus cursus sodales nec, sodales ut dui. </p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message own">
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ante suscipit bibendum ultricies. </p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit. </p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message own">
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ante suscipit bibendum ultricies. Praesent at nisi ut lorem vestibulum suscipit sed quis erat. </p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="icons">
+            <img src="./img.png" alt="" />
+            <img src="./camera.png" alt="" />
+            <img src="./mic.png" alt="" />
+          </div>
+          <input type="text" placeholder="Type a message..." value={text} onChange={e=>setText(e.target.value)}/>
+          <div className="emoji">
+            <img src="./emoji.png" alt="" onClick={()=>setOpen(prev=>!prev)}/>
+            <div className="picker">
+              <EmojiPicker  open={open} onEmojiClick={handleEmoji}/>
+            </div>
+          </div>
+          <button className="sendButton">Send</button>
+        </div>
+      </div>
+    )
+}
+
+export default Chat
